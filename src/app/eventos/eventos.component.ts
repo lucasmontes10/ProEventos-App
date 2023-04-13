70221f86cf3eebcb1d0c466d6bc44785dacab220
Lucas Montes <lumoliveira@icatuseguros.com.br>
2023-04-13 10:22:53 -0300

    Parte de Contato com o Back - Parte do Front

---
+++ src/app/eventos/eventos.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+})
+export class EventosComponent implements OnInit {
+  public eventos: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:7107/api/Evento').subscribe(
+      (response) => (this.eventos = response),
+      (erro) => console.log(erro)
+    );
+  }
+}
